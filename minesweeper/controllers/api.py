6146927c4373faa09dcb9780786e7e73ca1784bb
Victor Guo <victor.guoyu@hotmail.com>
2015-09-04 20:19:16 -0400

    rest api with cornice test

--- minesweeper/controllers/api.py
+++ minesweeper/controllers/api.py
@@ -1,13 +1,38 @@
-from cornice import Service
-
-game = Service(name='game api', path='/game', description='game api service')
-
-
-@game.get()
-def create_game(request):
-    return {'Hello': 'world'}
+import os
+import binascii
+from cornice.resource import resource, view
 
 
-@game.post()
-def hello_post(request):
-    return{'msg': 'This is the hello world from post service'}
+@resource(collection_path='/game', path='/game/{id}')
+class GameResource:
+    _games = []
+
+    def __init__(self, request):
+        self.request = request
+
+    # create new game
+    def collection_post(self):
+        return {
+            "id": _create_id(),
+            "msg": "create new game with collection post",
+        }
+
+    @view(renderer='json')
+    def get(self):
+        game_id = self.request.matchdict['id']
+        return {
+            'id': game_id,
+            'msg': 'get existing game',
+        }
+
+    @view(renderer='json')
+    def post(self):
+        game_id = self.request.matchdict['id']
+        return {
+            "id": game_id,
+            'msg': 'post existing game',
+        }
+
+
+def _create_id():
+    return binascii.b2a_hex(os.urandom(20))
